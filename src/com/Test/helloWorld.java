97c257119d62e84f8ba2ee07afa80c6174a99652
EthanRoyal <1409811823@qq.com>
2023-08-24 17:57:12 +0800

    Initial commit

---
+++ src/com/Test/helloWorld.java
@@ -0,0 +1,10 @@
+package com.Test;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        String string=new String("776");
+/*        switch (string){
+            case "-"->{}
+        }*/
+    }
+}
